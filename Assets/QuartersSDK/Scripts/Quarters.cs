77d1b49f5f70079176ab81faa399411b20109ae0
Greg Lukosek <lukos86@gmail.com>
2019-07-09 07:19:15 +0100

    fixed issue with missing access token when transferring just after login fixed case where request id might be duplicated due to failed auto approve transfer

--- Assets/QuartersSDK/Scripts/Quarters.cs
+++ Assets/QuartersSDK/Scripts/Quarters.cs
@@ -778,6 +778,21 @@ namespace QuartersSDK {
 
 
 
+        private void AddOrSwapAPITransferRequest(TransferAPIRequest request) {
+
+            for (int i = 0; i < currentTransferAPIRequests.Count; i++) {
+
+                if (currentTransferAPIRequests[i].requestId == request.requestId) {
+                    currentTransferAPIRequests[i] = request;
+                    return;
+                }
+                
+            }
+            
+            currentTransferAPIRequests.Add(request);
+            
+        }
+
 
 
 
@@ -825,7 +840,7 @@ namespace QuartersSDK {
 
                 request.requestId = transferRequest.id;
                 Debug.Log("request id is: " + transferRequest.id);
-                currentTransferAPIRequests.Add(request);
+                AddOrSwapAPITransferRequest(request);
 
                 //continue outh forward
                 string url = QUARTERS_URL + "/requests/" + transferRequest.id + "?inline=true" + "&redirect_uri=" + URL_SCHEME;
@@ -860,8 +875,18 @@ namespace QuartersSDK {
         
             Debug.Log("CreateAutoApprovedTransfer");
 
+
+            if (string.IsNullOrEmpty(session.AccessToken)) {
+                yield return StartCoroutine(GetAccessToken(null, null));
+            }
+            
+            
+            
+
             Dictionary<string, string> headers = new Dictionary<string, string>(AuthorizationHeader);
             headers.Add("Authorization", "Bearer " + session.AccessToken);
+            
+            Debug.Log("Headers: " + JsonConvert.SerializeObject(headers));
 
 
             Dictionary<string, object> data = new Dictionary<string, object>();
@@ -896,7 +921,7 @@ namespace QuartersSDK {
 
                 request.requestId = transferRequest.id;
                 Debug.Log("request id is: " + transferRequest.id);
-                currentTransferAPIRequests.Add(request);
+                AddOrSwapAPITransferRequest(request);
 
                 StartCoroutine(AutoApproveTransfer(request));
 
@@ -909,6 +934,7 @@ namespace QuartersSDK {
 
 
         private IEnumerator AutoApproveTransfer(TransferAPIRequest request) {
+            
             
             bool areAccountsDone = false;
             string accountsLoadingError = "";
@@ -1051,6 +1077,7 @@ namespace QuartersSDK {
 
         private void ProcessDeepLink(bool isExternalBrowser, Dictionary<string, string> urlParams) {
 
+            Debug.Log("ProcessDeepLink " + JsonConvert.SerializeObject(urlParams));
 
             foreach (KeyValuePair<string,string> urlParam in urlParams) {
                 Debug.Log(urlParam.Key + " : " + urlParam.Value);
