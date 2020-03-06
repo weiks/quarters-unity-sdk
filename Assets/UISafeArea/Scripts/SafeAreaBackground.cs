00d7d0e2814e6c1e4a854d3145b10ee92304cc9a
Artur Lukosek <artur@unitydevelopers.co.uk>
2020-03-06 13:11:05 +0100

    Implemented SafeArea to Menu and Shop views

---
+++ Assets/UISafeArea/Scripts/SafeAreaBackground.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SafeAreaBackground : MonoBehaviour, ISafeAreaElement {
+
+	public void SafeAreaChanged(UISafeArea safeArea, RectTransform canvasRectTransform)
+	{
+		RectTransform rectTransform = (RectTransform) this.transform;
+		
+		rectTransform.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Top, safeArea.topMarginRectSize, canvasRectTransform.rect.height);
+		rectTransform.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Right, safeArea.rightMarginRectSize, canvasRectTransform.rect.width);
+		rectTransform.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, -safeArea.leftMarginRectSize, canvasRectTransform.rect.width);
+		rectTransform.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Bottom, -safeArea.bottomMarginRectSize, canvasRectTransform.rect.height);
+	}
+}
