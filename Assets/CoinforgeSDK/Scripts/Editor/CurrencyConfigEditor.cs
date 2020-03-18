854879205d9f89066781db8e43d7171fed893dfd
Greg Lukosek <lukos86@gmail.com>
2020-03-18 15:24:55 +0000

    project restructure cleanup

---
+++ Assets/CoinforgeSDK/Scripts/Editor/CurrencyConfigEditor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(CurrencyConfig))]
+public class CurrencyConfigEditor : Editor
+{
+    public override void OnInspectorGUI() {
+        base.OnInspectorGUI();
+
+        CurrencyConfig config = (CurrencyConfig) target;
+
+        
+        GUIStyle errorStyle = new GUIStyle(EditorStyles.textField);
+        errorStyle.normal.textColor = Color.red;
+
+      
+        EditorGUILayout.Space();
+        foreach (char c in config.Code.ToCharArray()) {
+            if (Char.IsUpper(c)) {
+                EditorGUILayout.LabelField("Invalid currency code. Currency code must be lowercase", errorStyle);
+            }
+        }
+        
+        EditorGUILayout.Space();
+        
+        foreach (string productId in config.IAPProductIds) {
+            foreach (char c in productId.ToCharArray()) {
+                if (Char.IsUpper(c)) {
+                    EditorGUILayout.LabelField("Invalid product ID. Product Ids must be lowercase", errorStyle);
+                }
+            }
+        }
+        
+
+    }
+    
+    
+    
+}
