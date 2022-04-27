c61e08a7f6fe86adc47ca46ee0cf0357757b9832
Greg Lukosek <greg@unitydevelopers.co.uk>
2022-04-27 14:33:42 +0100

    removed obsolete old side menu
    simplifying currency config

--- Assets/QuartersSDK/Scripts/CurrencyConfig.cs
+++ Assets/QuartersSDK/Scripts/CurrencyConfig.cs
@@ -8,7 +8,6 @@ using UnityEngine.UI;
 
 
 namespace QuartersSDK {
-    [CreateAssetMenu(fileName = "DefaultCurrency", menuName = "Quarters/Currency", order = 1)]
     public class CurrencyConfig : ScriptableObject {
 
         public string APIBaseUrl;
@@ -17,56 +16,8 @@ namespace QuartersSDK {
         public string DisplayNamePlural;
 
         [Header("Branding")] public Sprite CurrencyIcon;
-        public Sprite CurrencyLogo;
-        public Color BackgroundsColor;
-        public Color BrandColor;
+        
 
-
-        [HideInInspector] [SerializeField] public List<int> IAPProductQuantities;
-
-
-        public List<string> IAPProductIds {
-            get {
-                List<string> result = new List<string>();
-
-                List<int> sortedQuantities = IAPProductQuantities;
-                sortedQuantities.Sort();
-
-                foreach (int productQuantity in sortedQuantities) {
-                    result.Add(GetProductId(productQuantity));
-                }
-
-                return result;
-            }
-        }
-
-        public string GetProductId(int quantity) {
-            return $"{Application.identifier}.{Code}.{quantity.ToString()}";
-        }
-
-        public int GetQuantity(string productId) {
-
-            string[] split = productId.Split('.');
-
-            if (split[split.Length - 2] == this.Code) {
-
-                string quantityString = split[split.Length - 1];
-
-                int quantity = -1;
-                if (int.TryParse(quantityString, out quantity)) {
-
-                    return quantity;
-                }
-                else {
-                    return -1;
-                }
-            }
-            else {
-                return -1;
-            }
-
-
-        }
     }
 }
 
