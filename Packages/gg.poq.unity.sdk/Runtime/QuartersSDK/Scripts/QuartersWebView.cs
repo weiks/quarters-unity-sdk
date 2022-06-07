0638f320155e35b151993ebfb1ba6f9728c51061
Greg Lukosek <lukos86@gmail.com>
2022-06-07 15:49:05 +0100

    QuartersWebView formatting

--- Packages/gg.poq.unity.sdk/Runtime/QuartersSDK/Scripts/QuartersWebView.cs
+++ Packages/gg.poq.unity.sdk/Runtime/QuartersSDK/Scripts/QuartersWebView.cs
@@ -1,34 +1,32 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using UnityEngine;
-using System.Runtime.InteropServices;
-using ImaginationOverflow.UniversalDeepLinking;
-using Newtonsoft.Json;
+﻿using ImaginationOverflow.UniversalDeepLinking;
 using UnityEditor;
-using UnityEngine.Serialization;
+using UnityEngine;
 
 namespace QuartersSDK {
     public class QuartersWebView : MonoBehaviour {
+        public delegate void OnCancelledDelegate();
 
         public delegate void OnDeepLinkDelegate(QuartersLink link);
+
         public static OnDeepLinkDelegate OnDeepLink;
 
-        public delegate void OnCancelledDelegate();
         public static OnCancelledDelegate OnCancelled;
-        
-        
-      
-        private bool renderEditorAuthorizationWindow = false;
-        
+
+
+        private readonly Color backgroundColor = new Color(255f / 19f, 255f / 34f, 255f / 43f);
+        private string editorAuthorizationUrl = string.Empty;
+
+
+        private bool renderEditorAuthorizationWindow;
+
         private UniWebView webView;
+
         public UniWebView WebView {
             get {
                 if (webView == null) {
-                    webView = this.gameObject.AddComponent<UniWebView>();
+                    webView = gameObject.AddComponent<UniWebView>();
                     webView.SetShowToolbar(false);
-                    
+
                     if (Application.isEditor) {
                         float editorScaleFactor = 0.5f;
                         webView.Frame = new Rect(0, 0, Screen.width * editorScaleFactor, Screen.height * editorScaleFactor);
@@ -43,9 +41,7 @@ namespace QuartersSDK {
 
                 return webView;
             }
-            set {
-                webView = value;
-            }
+            set => webView = value;
         }
 
         public void Init() {
@@ -53,15 +49,11 @@ namespace QuartersSDK {
         }
 
 
-        
-        
         private void OnDestroy() {
             DeepLinkManager.Instance.LinkActivated -= OnLinkActivated;
-            if (webView != null) {
-                webView.OnPageStarted -= WebViewOnOnPageStarted;
-            }
+            if (webView != null) webView.OnPageStarted -= WebViewOnOnPageStarted;
         }
-        
+
 
         //webview
         private void WebViewOnOnPageStarted(UniWebView webview, string url) {
@@ -75,30 +67,21 @@ namespace QuartersSDK {
             }
         }
 
-        
 
-        
-        
         //external universal link
         private void OnLinkActivated(LinkActivation linkActivation) {
             Debug.Log($"ApplicationOnDeepLinkActivated: {linkActivation.Uri} is valid deep link: {linkActivation.Uri.IsValidDeepLink()}");
             QuartersLink link = QuartersLink.Create(linkActivation.Uri);
-            
-            if (link.Uri.IsValidDeepLink()) {
-                //deep link opened
-                if (OnDeepLink != null) OnDeepLink(link);
-                    
-            }
+
+            if (link.Uri.IsValidDeepLink()) //deep link opened
+                if (OnDeepLink != null)
+                    OnDeepLink(link);
         }
 
-    
 
         public void OpenURL(string url, LinkType linkType) {
+            if (Application.isEditor && Application.platform != RuntimePlatform.WindowsEditor) linkType = LinkType.WebView;
 
-            if (Application.isEditor && Application.platform != RuntimePlatform.WindowsEditor) {
-                linkType = LinkType.WebView;
-            }
-            
             if (linkType == LinkType.WebView) {
                 WebView.Load(url);
                 WebView.Show();
@@ -113,19 +96,15 @@ namespace QuartersSDK {
             }
         }
 
-
-        private Color backgroundColor = new Color(255f / 19f, 255f / 34f, 255f / 43f);
-        private string editorAuthorizationUrl = string.Empty;
-        void OnGUI() {
+        private void OnGUI() {
             if (!renderEditorAuthorizationWindow) return;
-            
-            GUI.color = backgroundColor;
-            GUI.ModalWindow(0,  new Rect(0, Screen.height * 0.3f, Screen.width, Screen.height * 0.5f), DrawEditorAuthWindow, "Quarters Editor Authorization");
-        }
-        
 
-        void DrawEditorAuthWindow(int windowID) {
-            
+            GUI.color = backgroundColor;
+            GUI.ModalWindow(0, new Rect(0, Screen.height * 0.3f, Screen.width, Screen.height * 0.5f), DrawEditorAuthWindow, "Quarters Editor Authorization");
+        }
+
+
+        private void DrawEditorAuthWindow(int windowID) {
             EditorGUILayout.BeginVertical();
             GUI.color = Color.white;
 
@@ -135,18 +114,19 @@ namespace QuartersSDK {
             editorAuthorizationUrl = GUILayout.TextArea(editorAuthorizationUrl, GUILayout.ExpandHeight(true));
 
             GUILayout.BeginHorizontal();
-            if (GUILayout.Button("Cancel")) {
-                renderEditorAuthorizationWindow = false;
-            }
+            if (GUILayout.Button("Cancel")) renderEditorAuthorizationWindow = false;
+
             if (GUILayout.Button("Paste and Authorize")) {
                 editorAuthorizationUrl = EditorGUIUtility.systemCopyBuffer;
                 AuthorizeEditor();
                 renderEditorAuthorizationWindow = false;
             }
+
             if (GUILayout.Button("Authorize")) {
                 AuthorizeEditor();
                 renderEditorAuthorizationWindow = false;
             }
+
             GUILayout.EndHorizontal();
             EditorGUILayout.EndVertical();
         }
@@ -155,8 +135,5 @@ namespace QuartersSDK {
             QuartersLink link = QuartersLink.Create(editorAuthorizationUrl);
             if (OnDeepLink != null) OnDeepLink(link);
         }
-        
-
-
     }
 }
